1d7ea6fc1257557de25617f57407f5f762ef9557
stefan0xC <509385+stefan0xC@users.noreply.github.com>
2020-05-18 07:17:10 +0200

    remove test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use actix_web::{middleware, web, App, HttpRequest, HttpServer};
+use actix_web::{middleware, web, App, HttpServer};
 
-async fn index(req: HttpRequest) -> &'static str {
-    println!("REQ: {:?}", req);
+async fn index() -> &'static str {
     "Hello world!"
 }
 
@@ -20,31 +19,4 @@ async fn main() -> std::io::Result<()> {
     .bind("0.0.0.0:8080")?
     .run()
     .await
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use actix_web::dev::Service;
-    use actix_web::{http, test, web, App, Error};
-
-    #[actix_rt::test]
-    async fn test_index() -> Result<(), Error> {
-        let app = App::new().route("/", web::get().to(index));
-        let mut app = test::init_service(app).await;
-
-        let req = test::TestRequest::get().uri("/").to_request();
-        let resp = app.call(req).await.unwrap();
-
-        assert_eq!(resp.status(), http::StatusCode::OK);
-
-        let response_body = match resp.response().body().as_ref() {
-            Some(actix_web::body::Body::Bytes(bytes)) => bytes,
-            _ => panic!("Response error"),
-        };
-
-        assert_eq!(response_body, r##"Hello world!"##);
-
-        Ok(())
-    }
 }
