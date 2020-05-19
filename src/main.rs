4cb830ff1d4b5698cabca46eca06cfddf14bb567
stefan0xC <509385+stefan0xC@users.noreply.github.com>
2020-05-19 14:46:22 +0200

    add serde and test json extractor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,15 @@
-use actix_web::{middleware, web, App, HttpServer};
+use actix_web::{middleware, web, App, HttpServer, Result};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Person {
+    id: u32,
+    name: String,
+}
+
+async fn person(person: web::Path<Person>) -> Result<String> {
+    Ok(format!("Welcome {} {}!", person.name, person.id))
+}
 
 async fn index() -> &'static str {
     "Hello world!"
@@ -15,6 +26,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .service(web::resource("/index.html").to(|| async { "Hello world!" }))
             .service(web::resource("/").to(index))
+            .route("/people/{id}/{name}", web::get().to(person),)
     })
     .bind("0.0.0.0:8080")?
     .run()
